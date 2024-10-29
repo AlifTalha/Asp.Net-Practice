7aaa44a96dec32f5f26f737510513636dbc4b545
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-10-30 01:13:18 +0600

    Add files via upload

---
+++ Form/Controllers/RegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        [HttpGet]
+        public ActionResult Register()
+        {
+            ViewBag.First_Name = Request["First_name"];
+            ViewBag.Last_Name = Request["Last_name"];
+            ViewBag.Email = Request["Email"];
+            ViewBag.Phone_Number = Request["Phone_Number"];
+            ViewBag.Address = Request["Address"];
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(string First_name,string Last_Name,string Email,int Phone_Number,string Address)
+        {
+            ViewBag.First_Name = First_name;
+            ViewBag.Last_Name = Last_Name;
+            ViewBag.Email = Email;
+            ViewBag.Phone_Number = Phone_Number;
+            ViewBag.Address = Address;
+            return View();
+        }
+    }
+}
---
+++ Form/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Name = Request["Name"];
+            ViewBag.Id = Request["Id"];
+            ViewBag.Code=Request["Code"];
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string Name, int Id, int Code)
+        {
+            ViewBag.Name = Name;
+            ViewBag.Id = Id;
+            ViewBag.Code = Code;
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Create(FormCollection fc) {
+        //    ViewBag.Name = fc["Name"];
+        //    ViewBag.Id = fc["Id"];
+        //    ViewBag.Code=fc["Code"];
+
+        //    return View();
+    
+    }
+}
