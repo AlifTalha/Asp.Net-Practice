8206283122a49d35a46a0730acd90856b0787cc8
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-10-31 20:22:43 +0600

    Add files via upload

---
+++ Lab2Class/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Lab2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Student());            
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(s);
+        }
+    }
+}
---
+++ Lab2Class/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab2.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage ="Please Provide Name..")]
+        [StringLength(20)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1,20,ErrorMessage ="ID must be  between 1 to 20")]
+        public int ID { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
