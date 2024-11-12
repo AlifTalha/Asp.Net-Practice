bb7eda99f58dab2522f1843324ed88cdca3e0628
ALIF HOSSAIN TALHA <hossainalif696@gmail.com>
2024-11-12 18:41:49 +0600

    Add new folder and files

---
+++ DepartmentDTO/Controllers/DepartmentController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DepartmentDTO.DTOs;
+using DepartmentDTO.EF;
+
+namespace DepartmentDTO.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        StudentDTOEntities db = new StudentDTOEntities(); 
+        public static Department Convert(DepartDTO d)
+        {
+            return new Department()
+            {
+                Id = d.Id,
+                Name = d.Name,
+            };
+        }
+        public static DepartDTO Convert(Department d)
+        {
+            return new DepartDTO()
+            {
+                Id = d.Id,
+                Name = d.Name,
+            };
+        }
+        public static List<DepartDTO> Convert(List<Department> data)
+        {
+            var list = new List<DepartDTO>();
+            foreach (var d in data)
+            {
+                list.Add(Convert(d));
+            }
+            return list;
+        }
+        public ActionResult List()
+        {
+            var data = db.Departments.ToList();
+            return View(Convert(data));
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new DepartDTO());
+        }
+        [HttpPost]
+        public ActionResult Create(DepartDTO d)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = new Department()
+                {
+                    Name = d.Name,
+                };
+                db.Departments.Add(data);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(d);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = db.Departments.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Convert(data));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(DepartDTO d)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = db.Departments.Find(d.Id);
+                if (data != null)
+                {
+                    data.Name = d.Name;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("List");
+            }
+            return View(d);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var data = db.Departments.Find(id);
+            return View(Convert(data));
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var data = db.Departments.Find(id);
+            return View(Convert(data));
+        }
+        [HttpPost]
+        public ActionResult Delete(int Id, string dcsn)
+        {
+            if(dcsn.Equals("Yes"))
+            {
+                var data = db.Departments.Find(Id);
+                db.Departments.Remove(data);
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ DepartmentDTO/DTOs/StudentDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DepartmentDTO.DTOs
+{
+    public class StudentDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(50, ErrorMessage = "First name should not exceed 50 characters.")]
+        public string FName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(50, ErrorMessage = "Last name should not exceed 50 characters.")]
+        public string LName { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        public DateTime Dob { get; set; }
+
+        [Range(0.0, 4.0, ErrorMessage = "Enter a valid CGPA between 0.0 and 4.0.")]
+        public double Cgpa { get; set; }
+    }
+}
