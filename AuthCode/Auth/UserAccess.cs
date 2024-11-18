95976c5797ac5fa68bd2e6aea67dddb87ea63305
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-11-19 03:42:13 +0600

    Add files via upload

---
+++ AuthCode/Auth/UserAccess.cs
@@ -0,0 +1,24 @@
+﻿using AuthCode.EF;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthCode.Auth
+{
+    public class UserAccess : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var user = (User)httpContext.Session["user"];
+            if (user != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Account/Login");
+        }
+    }
+}
