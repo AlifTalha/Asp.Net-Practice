3b6e0cd221099854696e6cba18d36e646f31f3dc
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-11-25 22:01:28 +0600

    Add files via upload

---
+++ Mid_Demo/DTOs/ChannelDTO.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mid_Demo.DTO
+{
+    public class ChannelDTO
+    {
+        [Key]
+        public int ChannelId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Channel Name cannot exceed 100 characters.")]
+        public string ChannelName { get; set; }
+
+        [Required]
+        [Range(1900, 9999, ErrorMessage = "Established Year must be between 1900 and the current year.")]
+        public int EstablishedYear { get; set; }
+
+        public static ValidationResult ValidateEstablishedYear(int year, ValidationContext context)
+        {
+            var currentYear = DateTime.Now.Year;
+            if (year < 1900 || year > currentYear)
+            {
+                return new ValidationResult($"Established Year must be between 1900 and {currentYear}.");
+            }
+            return ValidationResult.Success;
+        }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Country cannot exceed 50 characters.")]
+        public string Country { get; set; }
+    }
+}
---
+++ Mid_Demo/DTOs/ProgramDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mid_Demo.DTOs
+{
+    public class ProgramDTO
+    {
+        [Key]
+        public int ProgramId { get; set; }
+
+        [Required]
+        [StringLength(150, ErrorMessage = "Program Name cannot exceed 150 characters.")]
+        public string ProgramName { get; set; }
+
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "TRP Score must be between 0.0 and 10.0.")]
+        public decimal TRPScore { get; set; }
+
+        [Required]
+        public int ChannelId { get; set; } 
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan AirTime { get; set; }
+    }
+}
