26393e9cd95681d9ee118a862de80e8fca0c9c55
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-12-12 15:59:17 +0600

    Add files via upload

---
+++ Final/Intro EF DEMO/EF/UMSContext.cs
@@ -0,0 +1,16 @@
+﻿using IntroEFC.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IntroEFC.EF
+{
+    public class UMSContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}
